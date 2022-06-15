63b74bc7b1667793fd75ae116af4299b4f112b73
Lara <lserradi@alumnos.unex.es>
2022-06-15 18:57:39 +0200

    feature: add Adapter to show stocks list

---
+++ app/src/main/java/com/lara/s/lopez/stockapplication/adapter/StocksAdapter.kt
@@ -0,0 +1,45 @@
+package com.lara.s.lopez.stockapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lara.s.lopez.domain.model.Stock
+import com.lara.s.lopez.stock.util.OnItemClickListener
+import com.lara.s.lopez.stockapplication.R
+
+class StocksAdapter(private val stocks: List<Stock>) :
+    RecyclerView.Adapter<StocksAdapter.ViewHolder>() {
+
+    private lateinit var onItemClickListener: OnItemClickListener
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val name = view.findViewById<TextView>(R.id.name)
+        private val hot = view.findViewById<TextView>(R.id.hot)
+        fun bind(stock: List<Stock>, position: Int, listener: OnItemClickListener) {
+            name.text = stock[position].name
+            hot.text = stock[position].hot.toString()
+            itemView.setOnClickListener {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(stocks, position, onItemClickListener)
+    }
+
+    override fun getItemCount(): Int = stocks.size
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+}
