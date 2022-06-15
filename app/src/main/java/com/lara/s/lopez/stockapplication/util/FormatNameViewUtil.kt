d0066996e41bb28208703933df2512e7f50c2f67
Lara <lserradi@alumnos.unex.es>
2022-06-15 18:54:04 +0200

    feature: add util package

---
+++ app/src/main/java/com/lara/s/lopez/stockapplication/util/FormatNameViewUtil.kt
@@ -0,0 +1,28 @@
+package com.lara.s.lopez.stock.util
+
+import com.lara.s.lopez.domain.model.CategoryType
+import javax.inject.Inject
+
+open class FormatNameViewUtil @Inject constructor() {
+
+    fun formatNameStock(nameStock: String, nameView: String): String {
+        return StringBuilder(nameView).append(": ").append(nameStock).toString()
+    }
+
+    fun formatHotStock(hotStock: String, nameView: String): String {
+        return StringBuilder(nameView).append(": ").append(hotStock).toString()
+    }
+
+    fun formatRicCodeStock(ricCodeStock: String, nameView: String): String {
+        return StringBuilder(nameView).append(": ").append(ricCodeStock).toString()
+    }
+
+    fun formatCategoryStock(category: CategoryType, nameView: String): String {
+        val nameCategory = when (category) {
+            CategoryType.PRIMARY -> "Primary"
+            CategoryType.INDUSTRY_AND_ENERGY -> "Industry & Energy"
+            CategoryType.BUILDING -> "Building"
+        }
+        return StringBuilder(nameView).append(": ").append(nameCategory).toString()
+    }
+}
