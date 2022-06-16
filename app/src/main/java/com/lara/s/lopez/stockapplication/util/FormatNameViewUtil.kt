567b10a1bc0e40c32d3b6cc7eceacc8bcb79e981
Lara <lserradi@alumnos.unex.es>
2022-06-16 20:56:31 +0200

    fix: viewModel make the format of the stock

--- app/src/main/java/com/lara/s/lopez/stockapplication/util/FormatNameViewUtil.kt
+++ app/src/main/java/com/lara/s/lopez/stockapplication/util/FormatNameViewUtil.kt
@@ -5,24 +5,12 @@ import javax.inject.Inject
 
 open class FormatNameViewUtil @Inject constructor() {
 
-    fun formatNameStock(nameStock: String, nameView: String): String {
-        return StringBuilder(nameView).append(": ").append(nameStock).toString()
-    }
-
-    fun formatHotStock(hotStock: String, nameView: String): String {
-        return StringBuilder(nameView).append(": ").append(hotStock).toString()
-    }
-
-    fun formatRicCodeStock(ricCodeStock: String, nameView: String): String {
-        return StringBuilder(nameView).append(": ").append(ricCodeStock).toString()
-    }
-
-    fun formatCategoryStock(category: CategoryType, nameView: String): String {
+    fun formatCategoryStock(category: CategoryType): String {
         val nameCategory = when (category) {
             CategoryType.PRIMARY -> "Primary"
             CategoryType.INDUSTRY_AND_ENERGY -> "Industry & Energy"
             CategoryType.BUILDING -> "Building"
         }
-        return StringBuilder(nameView).append(": ").append(nameCategory).toString()
+        return StringBuilder(nameCategory).toString()
     }
 }
